618153943e7c9948256ae7a930de4f3810177396
Matt Watson <mattwat@gmail.com>
2021-08-11 11:28:45 -0400

    add link to github
    install prop-types
    
    add icon component
    
    add aria-label to gh link

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import { EventCard } from './event/event-card'
 import './app.scss'
 import { Layout } from 'antd'
 import Markdown, * as event from '../content/ai-for-networking.md'
+import { Icon } from './icons'
 
 const { Footer } = Layout
 
@@ -12,7 +13,17 @@ export const App = () => {
         <EventCard event={ event } markdown={ <Markdown /> } />
       </Layout>
       <Footer className="footer">
-        RENCI &copy; 2021
+        <div>RENCI &copy; 2021</div>
+        <div>
+          <a
+            href="https://github.com/RENCI/ai-for-networking-workshop/"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="View source for this page"
+          >
+            <Icon icon="github" fill="var(--color-unc-white)" className="github-icon" />
+          </a>
+        </div>
       </Footer>
     </div>
   )
