58bb5dba020af95a51377bb2f7a979d3e3aef70b
Matt Watson <mattwat@gmail.com>
2021-08-11 11:17:46 -0400

    resize agenda iframe

--- src/components/event/tabs/agenda.js
+++ src/components/event/tabs/agenda.js
@@ -6,7 +6,7 @@ export const AgendaTab = ({ url }) => {
   return (
     <Space direction="vertical">
       <Title level={ 2 }>Agenda</Title>
-      <iframe src={ url } height="1810" width="700"></iframe>
+      <iframe src={ url } height="1050" width="805"></iframe>
     </Space>
   )
 }
