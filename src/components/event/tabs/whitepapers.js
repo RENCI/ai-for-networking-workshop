6a73a87414ca785fe365e82c4a9c8b1cab1eae99
Matt Watson <mattwat@gmail.com>
2021-08-25 09:19:17 -0400

    add link to form if browser doesn't support iframes

--- src/components/event/tabs/whitepapers.js
+++ src/components/event/tabs/whitepapers.js
@@ -6,7 +6,13 @@ export const CallForWhitepapersTab = ({ url }) => {
   return (
     <Space direction="vertical">
       <Title level={ 2 }>Call for Whitepapers</Title>
-      <iframe src={ url } width="700" height="800" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+      <div className="iframe-wrapper">
+        <iframe src={ url } width="780" height="900" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+      </div>
+      <div className="form-note">
+        The <em>Call for Whitepapers</em> form should be embedded above, but
+        you may <a href={ url }>visit the form directly</a> if it is not showing.
+      </div>
     </Space>
   )
 }
